49f6f28b730a68c5a37919fc33b18077036246d5
anjoo91 <andrew.w.lew@gmail.com>
2023-08-07 08:00:24 -0400

    updated css styling

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,41 +1,44 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import userService from '../../utils/userService';
+import './LoginPage.css'; // Import the CSS file
 
 export default function LoginPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        
-        try {
-            await userService.login({ email, password });
-            navigate('/'); // Redirect to homepage after successful login
-        } catch (err) {
-            // Handle login errors here
-            alert('Login failed!');
-        }
-    };
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-    return (
+    try {
+      await userService.login({ email, password });
+      navigate('/'); // Redirect to homepage after successful login
+    } catch (err) {
+      // Handle login errors here
+      alert('Login failed!');
+    }
+  };
+
+  return (
+    <div className="login-container"> {/* Add the login-container div */}
+      <div className="login-form"> {/* Add the login-form div */}
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email: </label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Login</button>
+        </form>
         <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Email: </label>
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-            <div>
-                Don't have an account? <a href="/signup">Sign Up</a>
-            </div>
+          Don't have an account? <a href="/signup">Sign Up</a>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,43 +1,48 @@
+// SignupPage.jsx
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import userService from '../../utils/userService';
+import './SignupPage.css'; // Import the new CSS file
 
 export default function SignupPage() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const handleSignup = async (e) => {
-        e.preventDefault();
+  const handleSignup = async (e) => {
+    e.preventDefault();
 
-        try {
-            await userService.signup({ username, email, password });
-            navigate('/'); // Redirect to homepage after successful registration
-        } catch (err) {
-            // Handle signup errors here, e.g., email might be taken
-            alert(err.message);
-        }
-    };
+    try {
+      await userService.signup({ username, email, password });
+      navigate('/'); // Redirect to homepage after successful registration
+    } catch (err) {
+      // Handle signup errors here, e.g., email might be taken
+      alert(err.message);
+    }
+  };
 
-    return (
-        <div>
-            <h2>Sign Up</h2>
-            <form onSubmit={handleSignup}>
-                <div>
-                    <label>Username: </label>
-                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-                </div>
-                <div>
-                    <label>Email: </label>
-                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-                </div>
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="signup-container">
+      <div className="signup-form">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSignup}>
+          <div>
+            <label>Username: </label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label>Email: </label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 }
